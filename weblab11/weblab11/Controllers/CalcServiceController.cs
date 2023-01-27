1447f8c718f7579c97d9773183b18605af9ded23
Timur <rocketridestudio@gmail.com>
2023-01-27 19:21:26 +0700

    lab 11 done

---
+++ weblab11/weblab11/Controllers/CalcServiceController.cs
@@ -0,0 +1,45 @@
+﻿using lab_11.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace lab_11.Controllers
+{
+    public class CalcServiceController : Controller
+    {
+        private readonly ILogger<CalcServiceController> _logger;
+
+        public CalcServiceController(ILogger<CalcServiceController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult PassUsingModel()
+        {
+            var model = new ViewModel();
+
+            return View(model);
+        }
+        public IActionResult PassUsingViewData()
+        {
+            Random random = new Random();
+            ViewData["num1"] = random.Next(0, 11);
+            ViewData["num2"] = random.Next(0, 11);
+            return View();
+        }
+        public IActionResult PassUsingViewBag()
+        {
+            Random random = new();
+            ViewBag.num1 = random.Next(0, 11);
+            ViewBag.num2 = random.Next(0, 11);
+            return View();
+        }
+        public IActionResult AccessServiceDirectly()
+        {
+            return View();
+        }
+    }
+}
---
+++ weblab11/weblab11/Models/ViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace lab_11.Models
+{
+    public class ViewModel
+    {
+        public int num1;
+        public int num2;
+
+        public ViewModel()
+        {
+            Random random = new Random();
+            num1 = random.Next(0, 11);
+            num2 = random.Next(0, 11);
+        }
+    }
+}
